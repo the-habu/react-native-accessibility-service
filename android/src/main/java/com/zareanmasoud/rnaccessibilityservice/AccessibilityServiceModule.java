6c0ad8a1fc4682b72d5dfbdc98eb1a0f517af926
Andreas Bretschneider <geekazoid@geekazoid.at>
2021-08-28 23:55:26 +0200

    Fix service

--- android/src/main/java/com/zareanmasoud/rnaccessibilityservice/AccessibilityServiceModule.java
+++ android/src/main/java/com/zareanmasoud/rnaccessibilityservice/AccessibilityServiceModule.java
@@ -11,6 +11,7 @@ import android.provider.Settings;
 import android.content.pm.ServiceInfo;
 
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
